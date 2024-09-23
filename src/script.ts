5c089e45e0007b6e968a190561d410f20f9fb8c9
Arthur Vieira <arthur.devbr@gmail.com>
2024-09-23 09:39:48 +0100

    protect the function return with type string

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,6 @@
-let button = document.querySelector(".button");
+function firstLetterUpperCase(name: string): string{
+    let firstLetter = name.charAt(0).toUpperCase();
+    return firstLetter + name.substring(1);
+};
 
-button.addEventListener("click", ()=>{
-    alert("Oi!");
-});
+let nome = firstLetterUpperCase("Arthu");
